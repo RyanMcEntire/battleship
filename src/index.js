1a46111b0b2ce6a77adf5471781cd6e7f2fe2665
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-05-17 22:39:22 -0600

    add some start game modal features

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 import { buildPlayerBoard, buildOpponentBoard } from './ui/main-game-ui';
 import gameLoop from './controller/game-controller';
+import buildNewGameModal from './ui/start-game-ui';
 
 buildPlayerBoard();
 buildOpponentBoard();
+buildNewGameModal()
 gameLoop();
--- src/ui/start-game-ui.js
+++ src/ui/start-game-ui.js
@@ -1,13 +1,45 @@
 import bp from 'domweaver';
 
-const placeShipsGrid = bp('div', 'placeShipsGrid');
+const newGameModal = bp('div', 'new-game-modal');
+const placeShipsGrid = bp('div', 'place-ships-grid');
+const rotateButton = bp('button', 'rotate-button').addText('Rotate');
 
 function writeGrid(parent) {
-  for (let i = 0; i <= 9; i += 1) {
-    for (let j = 1; j <= 10; j += 1) {
+  const letters = 'ABCDE';
+  for (let i = 0; i <= 4; i += 1) {
+    for (let j = 1; j <= 5; j += 1) {
       parent.addChild(bp('div', `${letters[i]}${j}`));
     }
   }
 }
 
-writeGrid(placeShipsGrid)
+writeGrid(placeShipsGrid);
+
+const shipChoiceContainer = bp('div', 'ship-choice-container');
+
+function attachShips(name, size) {
+  const shipChoice = bp('div', 'ship-choice');
+  const shipName = bp('div', 'ship-name').addText(name);
+  shipChoice.addChild(shipName);
+  const shipSize = bp('div', 'ship-size');
+  for (let i = 0; i < size; i += 1) {
+    shipSize.addChild(bp('div', 'size-unit'));
+  }
+  shipChoice.addChild(shipSize);
+  shipChoiceContainer.addChild(shipChoice);
+}
+
+attachShips('CARRIER', 5);
+attachShips('BATTLESHIP', 4);
+attachShips('CRUISER', 3);
+attachShips('SUBMARINE', 3);
+attachShips('DESTROYER', 2);
+
+newGameModal
+  .addChild(placeShipsGrid)
+  .addChild(rotateButton)
+  .addChild(shipChoiceContainer);
+
+export default function buildNewGameModal() {
+  document.body.appendChild(newGameModal.build());
+}
